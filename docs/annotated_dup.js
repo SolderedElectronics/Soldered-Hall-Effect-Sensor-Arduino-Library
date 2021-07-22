8a6e8b5cb52d5b1ec3faa393248795d0442bb5ca
Docs Robot <robot@example.com>
2021-07-22 06:37:45 +0000

    Committing documentation changes

--- docs/annotated_dup.js
+++ docs/annotated_dup.js
@@ -1,9 +1,10 @@
 var annotated_dup =
 [
     [ "Repository Contents", "md__r_e_a_d_m_e.html#autotoc_md1", null ],
-    [ "Hardware", "md__r_e_a_d_m_e.html#autotoc_md2", null ],
+    [ "Hardware design", "md__r_e_a_d_m_e.html#autotoc_md2", null ],
     [ "Documentation", "md__r_e_a_d_m_e.html#autotoc_md3", null ],
-    [ "Open-source license", "md__r_e_a_d_m_e.html#autotoc_md4", null ],
+    [ "About Soldered", "md__r_e_a_d_m_e.html#autotoc_md4", null ],
+    [ "Open-source license", "md__r_e_a_d_m_e.html#autotoc_md5", null ],
     [ "EasyC", "class_easy_c.html", "class_easy_c" ],
     [ "Hall_Effect", "class_hall___effect.html", "class_hall___effect" ]
 ];
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hall_20effect_20board_20arduino_20library_20by_20soldered_41',['Hall Effect Board Arduino library by Soldered',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['hall_20effect_20arduino_20library_41',['Hall Effect Arduino library',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
