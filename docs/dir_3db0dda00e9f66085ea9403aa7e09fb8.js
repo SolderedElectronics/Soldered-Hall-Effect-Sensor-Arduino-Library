4bc9f1b64db4be8aee56472b5e3a9046acbc24af
Docs Robot <robot@example.com>
2022-02-17 07:03:37 +0000

    Committing documentation changes

---
+++ docs/dir_3db0dda00e9f66085ea9403aa7e09fb8.js
@@ -0,0 +1,7 @@
+var dir_3db0dda00e9f66085ea9403aa7e09fb8 =
+[
+    [ "easyC.cpp", "easy_c_8cpp.html", null ],
+    [ "easyC.h", "easy_c_8h.html", [
+      [ "HALL_EFFECT_SOLDERED_LIBS::EasyC", "class_h_a_l_l___e_f_f_e_c_t___s_o_l_d_e_r_e_d___l_i_b_s_1_1_easy_c.html", "class_h_a_l_l___e_f_f_e_c_t___s_o_l_d_e_r_e_d___l_i_b_s_1_1_easy_c" ]
+    ] ]
+];
--- docs/files_dup.js
+++ docs/files_dup.js
@@ -1,11 +1,4 @@
 var files_dup =
 [
-    [ "Hall-Effect-SOLDERED.cpp", "_hall-_effect-_s_o_l_d_e_r_e_d_8cpp.html", null ],
-    [ "Hall-Effect-SOLDERED.h", "_hall-_effect-_s_o_l_d_e_r_e_d_8h.html", [
-      [ "Hall_Effect", "class_hall___effect.html", "class_hall___effect" ]
-    ] ],
-    [ "easyC.cpp", "easy_c_8cpp.html", null ],
-    [ "easyC.h", "easy_c_8h.html", [
-      [ "HALL_EFFECT_SOLDERED_LIBS::EasyC", "class_h_a_l_l___e_f_f_e_c_t___s_o_l_d_e_r_e_d___l_i_b_s_1_1_easy_c.html", "class_h_a_l_l___e_f_f_e_c_t___s_o_l_d_e_r_e_d___l_i_b_s_1_1_easy_c" ]
-    ] ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
