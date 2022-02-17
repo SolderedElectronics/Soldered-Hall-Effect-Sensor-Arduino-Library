5ffb1769121690e10be3d02dcf1ac7e4fc5f14e2
Main Push Robot <robot@example.com>
2022-02-17 12:18:48 +0000

    Committing dev changes

--- src/Hall-Effect-SOLDERED.h
+++ src/Hall-Effect-SOLDERED.h
@@ -15,7 +15,7 @@
 #include "Arduino.h"
 #include "libs/Generic-easyC/easyC.hpp"
 
-class Hall_Effect : public HALL_EFFECT_SOLDERED_LIBS::EasyC
+class Hall_Effect : public EasyC
 {
   public:
     // Hall_Effect(int _pin);
