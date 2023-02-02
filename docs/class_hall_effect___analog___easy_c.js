d94badc9d50bbc58b28fe39b3cf93e5d8093d1e6
Docs Robot <robot@example.com>
2023-02-02 13:29:13 +0000

    Committing documentation changes

--- docs/class_hall_effect___analog___easy_c.js
+++ docs/class_hall_effect___analog___easy_c.js
@@ -2,6 +2,6 @@ var class_hall_effect___analog___easy_c =
 [
     [ "HallEffect_Analog_EasyC", "class_hall_effect___analog___easy_c.html#a43a0aa196c6701f2484e0d17dd6fd213", null ],
     [ "getMilliTeslas", "class_hall_effect___analog___easy_c.html#aa94d96ee929fef5b398706a211c302ff", null ],
-    [ "getRawReading", "class_hall_effect___analog___easy_c.html#a30114fcd2324ac471f546702cf1f6369", null ],
+    [ "getRawReading", "class_hall_effect___analog___easy_c.html#a5bc9c963f7a9fea1099b09170de91b71", null ],
     [ "initializeNative", "class_hall_effect___analog___easy_c.html#a4e103aaa2eeedee7a98d226650f5eb74", null ]
 ];
