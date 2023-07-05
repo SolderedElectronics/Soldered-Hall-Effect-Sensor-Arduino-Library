148b14877d83ba0982d925634d2a52eaabab7187
Docs Robot <robot@example.com>
2023-07-05 05:46:54 +0000

    Committing documentation changes

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['pin_0',['pin',['../class_hall_effect___analog.html#a69c9d9acaf5386ae7f7ec64ec0f97c60',1,'HallEffect_Analog::pin()'],['../class_hall_effect___digital.html#ad0e82e37192cf645f53175d70024bb4a',1,'HallEffect_Digital::pin()']]]
+  ['pin_0',['pin',['../class_hall_effect___analog.html#a69c9d9acaf5386ae7f7ec64ec0f97c60',1,'HallEffect_Analog::pin'],['../class_hall_effect___digital.html#ad0e82e37192cf645f53175d70024bb4a',1,'HallEffect_Digital::pin']]]
 ];
